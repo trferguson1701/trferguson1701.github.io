b73cae021184174983c4748cb04c867f5ef34346
Taylor <56098777+trferguson1701@users.noreply.github.com>
2019-11-16 17:57:01 -0500

    TextBox Functionality
    Added in the fucntionality to the GUI text boxes.

--- ZooAnimalFrontEnd.py
+++ ZooAnimalFrontEnd.py
@@ -5,6 +5,8 @@ Species, Name, Age, Feeding Schedule
 
 from tkinter import *
 
+# Window outline and creation
+
 window=Tk()
 
 window.wm_title("ZooAnimals")
@@ -20,6 +22,25 @@ l3.grid(row=1,column=0)
 
 l4=Label(window,text="Feeding Schedule")
 l4.grid(row=1,column=2)
+
+# Text box creation and specs
+
+Species_text=StringVar()
+e1=Entry(window, textvariable=Species_text)
+e1.grid(row=0, column=1)
+
+Name_text=StringVar()
+e2=Entry(window, textvariable=Name_text)
+e2.grid(row=0, column=3)
+
+Age_text=StringVar()
+e3=Entry(window, textvariable=Age_text)
+e3.grid(row=1, column=1)
+
+Feed_text=StringVar()
+e4=Entry(window, textvariable=Feed_text)
+e4.grid(row=1, column=3)
+
 
 
 

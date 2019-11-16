8a9a279247684d755108125931eb8c11d7fe2f60
Taylor <56098777+trferguson1701@users.noreply.github.com>
2019-11-15 22:00:46 -0500

    Added 4 widgets
    Added the 4 widgets to search by in the frontend GUI

--- ZooAnimalFrontEnd.py
+++ ZooAnimalFrontEnd.py
@@ -1,8 +1,26 @@
+"""
+Front End program to handle animal information-
+Species, Name, Age, Feeding Schedule
+"""
+
 from tkinter import *
 
 window=Tk()
 
 window.wm_title("ZooAnimals")
+
+l1=Label(window,text="Species")
+l1.grid(row=0,column=0)
+
+l2=Label(window,text="Name")
+l2.grid(row=0,column=2)
+
+l3=Label(window,text="Age")
+l3.grid(row=1,column=0)
+
+l4=Label(window,text="Feeding Schedule")
+l4.grid(row=1,column=2)
+
 
 
 window.mainloop()

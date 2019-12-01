8924f7403566598f1412aa0f5b052eca459a0a1f
Taylor <56098777+trferguson1701@users.noreply.github.com>
2019-12-01 17:15:36 -0500

    Update ZooAnimalFrontEnd.py
    tuple debug on listbox

--- ZooAnimalFrontEnd.py
+++ ZooAnimalFrontEnd.py
@@ -7,17 +7,20 @@ from tkinter import *
 import ZooAnimalBackend
 
 def get_selected_row(event):
-    global selected_tuple
-    index=result1.curselection()[0]
-    selected_tuple=result1.get(index)
-    e1.delete(0,END)
-    e1.insert(END,selected_tuple[1])
-    e2.delete(0,END)
-    e2.insert(END,selected_tuple[2])
-    e3.delete(0,END)
-    e3.insert(END,selected_tuple[3])
-    e4.delete(0,END)
-    e4.insert(END,selected_tuple[4])
+    try:
+        global selected_tuple
+        index=result1.curselection()[0]
+        selected_tuple=result1.get(index)
+        e1.delete(0,END)
+        e1.insert(END,selected_tuple[1])
+        e2.delete(0,END)
+        e2.insert(END,selected_tuple[2])
+        e3.delete(0,END)
+        e3.insert(END,selected_tuple[3])
+        e4.delete(0,END)
+        e4.insert(END,selected_tuple[4])
+    except IndexError:
+        pass
     
 
 #View all functionality

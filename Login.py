eab2778813ba7976983c84a947e458c7cf98a703
Taylor <56098777+trferguson1701@users.noreply.github.com>
2019-12-15 16:18:14 -0500

    Updated Login
    Added more features to the login function

--- Login.py
+++ Login.py
@@ -1,21 +1,42 @@
 from tkinter import *
+from functools import partial
 
+
+def validateCred(username, password):
+    print("Username:", username.get())
+    print("Password:", password.get())
+    return
+
+
+
+#Window Specifications
 
 window=Tk()
+window.geometry('400x150')
+window.title("ZooAnimals Login")
 
-l1=Label(window,text='User:')
-l2=Label(window,text='Password:')
+#Text box Creation for user 
+l1=Label(window,text="Username:")
+l1.grid(row=0, column=0)
+username = StringVar()
+usernameEntry = Entry(window, textvariable=username)
+usernameEntry.grid(row=0, column=1)
 
-e1=Entry(window,textvariable=StringVar())
-e2=Entry(window,show='*',textvariable=StringVar())
+#Text box Creation for password
+l2=Label(window,text="Password:")
+l2.grid(row=1, column=0)
+password = StringVar()
+passwordEntry= Entry(window, textvariable=password, show='*')
+passwordEntry.grid(row=1, column=1)
 
-b1=Button(window,text='Log in')
+#validation creation
+validateCred = partial(validateCred, username, password)
 
-l1.pack()
-e1.pack()
-l2.pack()
-e2.pack()
-b1.pack()
+#Button Creation
+
+b1=Button(window,text="login",command=window.destroy)
+b1.grid(row=4,column=1)
+    
 
 window.mainloop()
 
